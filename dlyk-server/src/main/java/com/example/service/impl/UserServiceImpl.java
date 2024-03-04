9308f726a29e24b648b579078ea2ca5be598865f
Joey <headmastereggy@gmail.com>
2024-03-04 11:23:45 +0800

    🔨 chore: refactor user detail form to display create and edit user details

--- dlyk-server/src/main/java/com/example/service/impl/UserServiceImpl.java
+++ dlyk-server/src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -1,34 +1,87 @@
 package com.example.service.impl;
 
+import com.example.constant.Constants;
+import com.example.manager.RedisManager;
+import com.example.mapper.TPermissionMapper;
+import com.example.mapper.TRoleMapper;
 import com.example.mapper.TUserMapper;
 import com.example.model.TUser;
+import com.example.query.UserQuery;
 import com.example.service.UserService;
+import com.example.utils.JWTUtils;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import jakarta.annotation.Resource;
+import org.springframework.beans.BeanUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import com.example.constant.Constants;
+
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
-
     @Resource
     private TUserMapper tUserMapper;
+    @Resource
+    private PasswordEncoder passwordEncoder;
+    @Resource
+    private TRoleMapper tRoleMapper;
+    @Resource
+    private RedisManager redisManager;
+    @Resource
+    private TPermissionMapper tPermissionMapper;
+
+    @Override
+    public int saveUser(UserQuery userQuery) {
+        TUser tUser = new TUser();
+
+        //把UserQuery对象里面的属性数据复制到TUser对象里面去(复制要求：两个对象的属性名相同，属性类型要相同，这样才能复制)
+        BeanUtils.copyProperties(userQuery, tUser);
+
+        tUser.setLoginPwd(passwordEncoder.encode(userQuery.getLoginPwd())); //密码加密
+        tUser.setCreateTime(new Date()); //创建时间
+
+        //登录人的id
+        Integer loginUserId = JWTUtils.parseUserFromJWT(userQuery.getToken()).getId();
+        tUser.setCreateBy(loginUserId); //创建人
+
+        return tUserMapper.insertSelective(tUser);
+    }
+
+    @Override
+    public int updateUser(UserQuery userQuery) {
+        return 0;
+    }
+
+    @Override
+    public int delUserById(Integer id) {
+        return 0;
+    }
+
+    @Override
+    public int batchDelUserIds(List<String> idList) {
+        return 0;
+    }
+
+    @Override
+    public List<TUser> getOwnerList() {
+        return null;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         TUser tUser = tUserMapper.selectByLoginAct(username);
-        if (tUser == null){
+        if (tUser == null) {
             throw new UsernameNotFoundException("登录账号不存在");
         }
         return tUser;
     }
 
     @Override
-    public PageInfo<TUser> getUserByPage(Integer current){
+    public PageInfo<TUser> getUserByPage(Integer current) {
         //1.设置PageHelper
         PageHelper.startPage(current, Constants.PAGE_SIZE);
         //2.查询
@@ -39,6 +92,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public TUser getUserById(Integer id) {
-        return tUserMapper.selectByPrimaryKey(id);
+        return tUserMapper.selectDetailById(id);
     }
+
+
 }
--- dlyk-server/src/main/java/com/example/web/UserController.java
+++ dlyk-server/src/main/java/com/example/web/UserController.java
@@ -1,16 +1,14 @@
 package com.example.web;
 
 import com.example.model.TUser;
+import com.example.query.UserQuery;
 import com.example.result.R;
 import com.example.service.UserService;
 import com.github.pagehelper.PageInfo;
 import jakarta.annotation.Resource;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -40,19 +38,62 @@ public class UserController {
         return R.OK();
     }
 
+    /**
+     * 用户列表分页查询
+     *
+     * @param current
+     * @return
+     */
+    @PreAuthorize(value = "hasAuthority('user:list')")
     @GetMapping(value = "/api/users")
-    public R userPage(@RequestParam(value = "current", required = false) Integer current){
+    public R userPage(@RequestParam(value = "current", required = false) Integer current) {
         //required = false 传参数 optional, true 传参数 must
-        if(current == null){
+        if (current == null) {
             current = 1;
         }
         PageInfo<TUser> pageInfo = userService.getUserByPage(current);
         return R.OK(pageInfo);
     }
 
+    @PreAuthorize(value = "hasAuthority('user:view')")
     @GetMapping(value = "/api/user/{id}")
-    public R userDetail(@PathVariable(value = "id") Integer id){
+    public R userDetail(@PathVariable(value = "id") Integer id) {
         TUser tuser = userService.getUserById(id);
         return R.OK(tuser);
+    }
+
+    /**
+     * 新增用户
+     *
+     * @param userQuery
+     * @return
+     */
+    @PreAuthorize(value = "hasAuthority('user:add')")
+    @PostMapping(value = "/api/user")
+    public R addUser(UserQuery userQuery, @RequestHeader(value = "Authorization") String token) {
+        userQuery.setToken(token);
+        int save = userService.saveUser(userQuery);
+        return save >= 1 ? R.OK() : R.FAIL();
+    }
+
+    /**
+     * 编辑用户
+     *
+     * @param userQuery
+     * @return
+     */
+    @PreAuthorize(value = "hasAuthority('user:edit')")
+    @PutMapping(value = "/api/user")
+    public R editUser(UserQuery userQuery, @RequestHeader(value = "Authorization") String token) {
+        userQuery.setToken(token);
+        int update = userService.updateUser(userQuery);
+        return update >= 1 ? R.OK() : R.FAIL();
+    }
+
+    @PreAuthorize(value = "hasAuthority('user:delete')")
+    @DeleteMapping(value = "/api/user/{id}")
+    public R delUser(@PathVariable(value = "id") Integer id) {
+        int del = userService.delUserById(id);
+        return del >= 1 ? R.OK() : R.FAIL();
     }
 }
